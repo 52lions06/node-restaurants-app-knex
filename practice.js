d296554f672abe2ad5306016502a507c45edea00
52lions06 <mckelvey.william@yahoo.com>
2017-07-06 16:01:34 -0500

    finished drills

--- practice.js
+++ practice.js
@@ -65,3 +65,53 @@ const knex = require('knex')({
 // 	.orderBy('name', 'asc')
 // 	.limit(5)
 // 	.then(results => console.log(results));
+
+//8. Create a restaurant 
+
+// knex.insert({name: 'Byte Cate', borough: 'Brooklyn', cuisine: 'coffee', address_building_number: '123', address_street: 'Atlantic Avenue',address_zipcode: '11231'}, 'id')
+//   .into('restaurants')
+//   .returning(['name','id'])
+//   .then(results => console.log(results));
+
+//9. Create a restuarant but return name and id
+
+// knex.insert({name: 'ULTIMATE TACO', borough: 'Manhattan', cuisine: 'mexican', address_building_number: '456', address_street: 'Heaven Street',address_zipcode: '10001'})
+//   .into('restaurants')
+//   .returning(['name','id'])
+//   .then(results => console.log(results));
+
+//10. Create three restaurants and return id and name
+
+// knex.insert([{name: 'ULTIMATE TACO', borough: 'Manhattan', cuisine: 'Mexican' , address_building_number: '456', address_street: 'Heaven Street',address_zipcode: '10001'
+// },
+//             {name: 'Luigi"s Pizza', borough: 'Brooklyn', cuisine: 'Pizza', address_building_number: '5568', address_street: 'Brooklyn Street',address_zipcode: '11111'
+// },
+//             {name: 'Bowser Boom Burgers', borough: 'Queens', cuisine: 'America', address_building_number: '23', address_street: 'Lebron Jordan Street',address_zipcode: '23232'
+// }])
+//   .into('restaurants')
+//   .returning(['name','id','cuisine'])
+//   .then(results => console.log(results));
+
+
+//11. Update the record 
+
+// knex('restaurants')
+// .where('nyc_restaurant_id', '30191841')
+// // .update({name:'DJ Reynolds Pub and Restaurant'})
+// .then(results => console.log(results));
+
+
+//12. Delete the id
+// knex.select()
+// .from('grades')
+// .where({id: 12, grade: 'A'})
+// .del({grade: 'A'})
+// .then(results => console.log(results));
+
+//13. Blocked Delete 
+
+knex.select()
+.from('restaurants')
+.where({id: 22})
+.del()
+.then(results => console.log(results));
