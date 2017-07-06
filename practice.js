cfe725bad435c903d31347fcf6e906e25c41f7db
daviddinnison <david.g.dinnison@gmail.com>
2017-07-06 14:21:14 -0400

    second commit

--- practice.js
+++ practice.js
@@ -5,11 +5,17 @@ const knex = require('knex')({
   },
 });
 
-knex.select('id', 'name', 'borough', 'cuisine')
-  .from('restaurants')
-  .where( { borough: 'Manhattan'})
-  .debug(true)
-  .then(results => console.log(JSON.stringify(results, null, 4)))
-  .catch( err => console.log( err ));
+// knex.select('id', 'name', 'borough', 'cuisine')
+//   .from('restaurants')
+//   .where( { borough: 'Manhattan'})
+//   .debug(true)
+//   .then(results => console.log(JSON.stringify(results, null, 4)))
+//   .catch( err => console.log( err ));
 
 //What did the debug do and how does the null / 4 effect the JSON
+
+
+//1. Get all restaurants
+knex.select()
+	.from('restaurants')
+	.then(results => console.log(results));
